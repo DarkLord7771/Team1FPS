6fbc839ecf41979cd66d746401205140bccf24a9
DarkLord7771 <134660640+DarkLord7771@users.noreply.github.com>
2024-03-10 14:04:55 -0400

    EnemyAI and GM

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour, IDamage
+{
+    [SerializeField] int HP;
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject Bullet;
+    [SerializeField] float shootRate;
+
+    bool isShooting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gamemanager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(gamemanager.instance.player.transform.position);
+
+        if (!isShooting)
+        {
+            StartCoroutine(shoot());
+        }
+
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(Bullet, shootPos.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    public void takeDamage(int amount)
+    {
+        HP -= amount;
+        StartCoroutine(flashRed());
+
+        if (HP <= 0)
+        {
+            gamemanager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+
+    }
+
+    IEnumerator flashRed()
+    {
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/gamemanager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemanager : MonoBehaviour
+{
+    public static gamemanager instance;
+
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+
+    public GameObject player;
+    public PlayerController playerScript;
+
+    public bool isPaused;
+
+    float timeScaleOrig;
+    int enemyCount;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<PlayerController>();
+        timeScaleOrig = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && menuActive == null)
+        {
+            statePaused();
+            menuActive = menuPause;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void statePaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+
+        // .None OR .Confined
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void stateUnPaused()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(isPaused);
+        menuActive = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        enemyCount += amount;
+        if (enemyCount <= 0)
+        {
+            statePaused();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+
+    }
+}
