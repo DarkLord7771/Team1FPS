ecd8194b5854c4827af7effe5708e3d3436d00f4
KaymanColvin <colvin.kayman@yahoo.com>
2024-03-10 13:58:38 -0800

    Updated Spawner
    Spawner will not spawn anything when another object is inside its trigger

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] GameObject enemy;
-    [SerializeField] Transform spawnPoint;
+    [SerializeField] GameObject spawnPoint;
     [SerializeField] float spawnDelay;
 
     bool spawnedEnemy;
+    bool spawnerEmpty;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnerEmpty = true;
     }
 
     // Update is called once per frame
@@ -29,9 +31,22 @@ public class Spawner : MonoBehaviour
     {
         spawnedEnemy = true;
 
-        Instantiate(enemy, spawnPoint.transform.position, Quaternion.identity);
+        if (spawnerEmpty)
+        {
+            Instantiate(enemy, spawnPoint.transform.position, Quaternion.identity);
+        }
 
         yield return new WaitForSeconds(spawnDelay);
         spawnedEnemy = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        spawnerEmpty = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        spawnerEmpty = true;
     }
 }
